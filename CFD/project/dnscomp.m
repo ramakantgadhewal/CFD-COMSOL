8d3fac3a4ad8f1217a0ddf8e1325c3771e37a428
hnlinder <henrik.linder.umu@gmail.com>
2021-12-26 19:16:12 +0100

    Working simulations, wall res good, simtime 44 min

---
+++ CFD/project/dnscomp.m
@@ -0,0 +1,35 @@
+clear 
+close all
+run import_uplus_dns.m
+run import_kplus_dns.m
+run import_epplus_dns.m
+run import_Pkplus_dns.m
+%%
+close all 
+figure 
+max_val = max(uplusDNS(:,2));
+plot(uplusDNS(:,1), uplusDNS(:,2)/max_val)
+hold on 
+grid on 
+
+%%
+% close all 
+% figure 
+max_val = max(kplusDNS(:,2));
+plot(kplusDNS(:,1), kplusDNS(:,2)/max_val)
+hold on 
+grid on
+%%
+% close all 
+% figure 
+max_val = max(PkplusDNS(:,2));
+plot(PkplusDNS(:,1), PkplusDNS(:,2)/max_val)
+hold on 
+grid on
+%%
+% close all 
+% figure 
+max_val = max(epplusDNS(:,2));
+plot(epplusDNS(:,1), epplusDNS(:,2)/max_val)
+hold on 
+grid on
